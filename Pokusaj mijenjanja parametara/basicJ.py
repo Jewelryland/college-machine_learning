11c422d197cd2ee95af62bf38e6db0034727f42e
akovacic <in.math0@gmail.com>
2014-06-25 16:14:46 +0200

    Neke probe razne...
    Brisanje nepotrebnih parametara u pozivu funkcije...

---
+++ Pokusaj mijenjanja parametara/basicJ.py
@@ -0,0 +1,27 @@
+from naive_bayes import NaiveBayes
+from svmM import SVM
+
+classifiers = [
+    NaiveBayes(selected = True),
+    SVM.Proba(selected = True),
+    SVM.Linear(selected = True),
+    SVM.Polynomial(selected = True),
+]
+
+for classifier in classifiers:
+
+    classifier.get_featuresets()
+    classifier.train_and_test(5)
+    # classifier.show_most_informative_features(10)
+
+    print "%.2f (accuracy)" % (classifier.accuracy)
+
+    document1 = ['a', ',wickedly', 'entertaining', 'sometimes', 'thrilling', 'adventure']
+    print "Classification: A wickedly entertaining, sometimes thrilling adventure."
+    print classifier.classify(classifier.sentiment_features(document1, classifier.words))
+
+    document2 = ['providing', 'a', 'riot', 'of', 'action', 'big', 'and', 'visually', 'opulent', 'but', 'oddly', 'lumbering', 'and', 'dull']
+    print "Classification: Providing a riot of action, big and visually opulent but oddly lumbering and dull."
+    print classifier.classify(classifier.sentiment_features(document2, classifier.words))
+
+    print "tp = %d, tn = %d, fp = %d, fn = %d" % classifier.confusion_matrix()
